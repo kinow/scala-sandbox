e8a8c30d25db71bc50955b04ae1f5c59264f5ea3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2012-06-01 23:28:40 -0300

    Initial commit

---
+++ src/sample/RangesInScala.scala
@@ -0,0 +1,15 @@
+package sample
+
+/**
+ *
+ * @author Bruno P. Kinoshita - http://www.kinoshita.eti.br
+ * @since 0.1
+ */
+class RangesInScala {
+
+  def main(args: Array[String]) {
+      val range = 0 to 9 by 2
+    println(range)
+  }
+
+}
